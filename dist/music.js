d7db609ea057e3f049b7fb95bc6ef85b00cc8fe4
LoveinSun <1974301247@qq.com>
2022-03-25 15:29:53 +0800

    Site updated: 2022-03-25 15:29:53

--- dist/music.js
+++ dist/music.js
@@ -1,14 +1,14 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    fixed: true,   #吸底模式
-    autoplay: false,  #自动播放
+    fixed: true,   
+    autoplay: false, 
     audio: [
 	 {
 		name: 'Five Hundred Miles',
 		artist: 'Justin Timberlake',
 		url: 'music.mp3',
 		cover: '?',
-	    lrc: '' #歌词播放并不好看，所以没有使用
+	    lrc: '' 
 	 },
     ]
 });
